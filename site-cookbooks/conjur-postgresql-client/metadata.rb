c4054f2b8b3d933e152cc5d2c045fa780ec5be6b
Kevin Gilpin <kgilpin@gmail.com>
2012-06-27 10:34:07 -0400

    added some site cookbooks with compliant metadata

---
+++ site-cookbooks/conjur-postgresql-client/metadata.rb
@@ -0,0 +1,20 @@
+maintainer        "Inscitiv, Inc."
+maintainer_email  "ops@inscitiv.com"
+license           "Apache 2.0"
+description       "Installs and configures postgresql client"
+version           "0.1.0"
+
+%w{ ubuntu debian fedora suse }.each do |os|
+  supports os
+end
+
+%w{redhat centos scientific}.each do |el|
+  supports el, ">= 6.0"
+end
+
+depends "postgresql", "= 0.99.4"
+
+attribute "postgresql/version",
+  :display_name => "postgresql database version",
+  :type => "string",
+  :required => "optional"
