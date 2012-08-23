f5c3355e3af442d72ed4c4fae573375782db69eb
Kevin Gilpin <kgilpin@gmail.com>
2012-08-23 11:36:50 -0400

    test for presence of chef_gem

--- spec/recipes/conjur_audit_shipper_spec.rb
+++ spec/recipes/conjur_audit_shipper_spec.rb
@@ -8,12 +8,16 @@ describe "conjur-audit-shipper" do
     end
   }
   let(:attrs) { conjur_attributes[:inscitiv][:aws_users][:server_events] }
+  before(:each) do
+    # Tame package installations
+    provider = mock(:provider).as_null_object
+    Chef::Provider::Package.stub(:new).and_return provider
+  end
+  it { should contain_chef_gem('aws-sdk') }
   it { 
     should contain_template("/opt/inscitiv/bin/authevent").
       with(:mode, "0755").
       with(:variables, :queue => attrs[:queue_url], :access_key => attrs[:access_key_id], :secret_key => attrs[:secret_access_key])
   }
-  it {
-    should contain_cookbook_file("/etc/rsyslog.d/75-authevent.conf")
-  }
+  it { should contain_cookbook_file("/etc/rsyslog.d/75-authevent.conf") }
 end
