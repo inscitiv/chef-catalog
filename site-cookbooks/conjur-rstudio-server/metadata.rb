1a5575dd41b03245fcf0b8022232454076d6b17a
Kevin Gilpin <kgilpin@gmail.com>
2012-09-08 16:41:35 -0400

    generated metadata.json

---
+++ site-cookbooks/conjur-rstudio-server/metadata.rb
@@ -0,0 +1,11 @@
+maintainer        "Inscitiv, Inc."
+maintainer_email  "ops@inscitiv.com"
+license           "Apache 2.0"
+description       "Installs and configures RStudio Server"
+version           "0.1.0"
+
+%w{ ubuntu debian }.each do |os|
+  supports os
+end
+
+depends "nginx"
