0dc262b29b95802d4585a05e78dbadd54186cdd0
Kevin Gilpin <kgilpin@gmail.com>
2012-09-09 08:31:48 -0400

    fixed perm/owner expectation

--- spec/recipes/conjur_audit_shipper_spec.rb
+++ spec/recipes/conjur_audit_shipper_spec.rb
@@ -16,7 +16,8 @@ describe "conjur-audit-shipper" do
   it { should contain_chef_gem('aws-sdk') }
   it { 
     should contain_template("/opt/inscitiv/bin/authevent").
-      with(:mode, "0755").
+      with(:mode, "0500").
+      with(:owner, 'syslog').
       with(:variables, :queue => attrs[:queue_url], :access_key => attrs[:access_key_id], :secret_key => attrs[:secret_access_key])
   }
   it { should contain_cookbook_file("/etc/rsyslog.d/75-authevent.conf") }
